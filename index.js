af190000a3fe48b4a8ac7b17980fd02371d0eda1
Alisson <53145093+alisnino@users.noreply.github.com>
2021-09-14 13:56:58 +0900

    Added navbar and intro content

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express")
-const path = require('path');
+const path = require('path')
 const app = express()
 const port = 3000
 
 // provide access to assets folder
 app.use(express.static(__dirname + "/assets"));
-app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap'));
+app.use(express.static(__dirname + "/images"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "theming-kit.html"))
+    res.sendFile(path.join(__dirname, "index.html"))
 })
 
+app.get("/template", (req, res) => {
+    res.sendFile(path.join(__dirname, "theming-kit.html"))
+})
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
